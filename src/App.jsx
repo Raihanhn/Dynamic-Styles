06d5676b735b95de1fd7208ecdc17ea3173581da
raihanhn <raihanhn20@gmail.com>
2022-10-28 23:01:36 +0600

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Glass from './components/Glass/Glass'
+
+function App() {
+
+  return (
+    <div className="App">
+     <Glass/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Glass/Glass.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import glass from './glass.css'
+
+const Glass = () => {
+  return (
+    <div className="container">
+        <div className="card">
+            <div className="content">
+                <h2>01</h2>
+                <h3>Card One</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus quam molestias quis nemo accusantium debitis a laudantium in temporibus modi.</p>
+                <a href="#">Read More</a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Glass
